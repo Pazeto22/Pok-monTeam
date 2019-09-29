1ddeec46eb3d413bd0a1da60950aee79fd82c903
Pazeto22 <gustavopazeto95@gmail.com>
2019-09-29 03:12:36 -0300

    V2.1
    Filtro funcionando;
    Mais pokémon adicionados;
    Diferetes tamanhos dependendo do pokémon;

--- js/index.js
+++ js/index.js
@@ -1,35 +1,39 @@
-// function allowDrop(ev) {
-//   ev.preventDefault();
-// }
-
-// function drag(ev) {
-//   ev.dataTransfer.setData("text", ev.target.id);
-// }
-
-// function drop(ev) {
-//   ev.preventDefault();
-//   var data = ev.dataTransfer.getData("text");
-//   ev.target.appendChild(document.getElementById(data));
-// }
-
-// Material Select Initialization
+let gen1 = document.querySelector(".gen1").innerHTML
+let gen2 = document.querySelector(".gen2").innerHTML
+let gen3 = document.querySelector(".gen3").innerHTML
+let gen4 = document.querySelector(".gen4").innerHTML
+let gen5 = document.querySelector(".gen5").innerHTML
+let gen6 = document.querySelector(".gen6").innerHTML
+let gen7 = document.querySelector(".gen7").innerHTML
 
 function pkm(Pokemon) {
   let pkm = document.getElementsByClassName("asas")
   let pkmE = Pokemon.value
+  let pkmSize = Pokemon.className.split(' ')[3];
+  let size = 100
+
+  if (pkmSize == "small") {
+    size = 200
+  } else if (pkmSize == "medium") {
+    size = 250
+  } else if (pkmSize == "abig") {
+    size = 275
+  } else if (pkmSize == "big") {
+    size = 300
+  }
 
   if (pkm[0].innerHTML.indexOf("img") == -1) {
-    return pkm[0].innerHTML = `<img src="./images/pkm/${pkmE}.png" height="250"; width="auto"; id="pok";>`
+    return pkm[0].innerHTML = `<img class="${pkmSize}" src="./images/pkm/${pkmE}.png" height="auto"; width="${size}"; id="pok";>`
   } else if (pkm[1].innerHTML.indexOf("img") == -1) {
-    return pkm[1].innerHTML = `<img src="./images/pkm/${pkmE}.png" height="250"; width="auto"; id="pok";>`
+    return pkm[1].innerHTML = `<img class="${pkmSize}" src="./images/pkm/${pkmE}.png" height="auto"; width="${size}"; id="pok";>`
   } else if (pkm[2].innerHTML.indexOf("img") == -1) {
-    return pkm[2].innerHTML = `<img src="./images/pkm/${pkmE}.png" height="250"; width="auto"; id="pok";>`
+    return pkm[2].innerHTML = `<img class="${pkmSize}" src="./images/pkm/${pkmE}.png" height="auto"; width="${size}"; id="pok";>`
   } else if (pkm[3].innerHTML.indexOf("img") == -1) {
-    return pkm[3].innerHTML = `<img src="./images/pkm/${pkmE}.png" height="250"; width="auto"; id="pok";>`
+    return pkm[3].innerHTML = `<img class="${pkmSize}" src="./images/pkm/${pkmE}.png" height="auto"; width="${size}"; id="pok";>`
   } else if (pkm[4].innerHTML.indexOf("img") == -1) {
-    return pkm[4].innerHTML = `<img src="./images/pkm/${pkmE}.png" height="250"; width="auto"; id="pok";>`
+    return pkm[4].innerHTML = `<img class="${pkmSize}" src="./images/pkm/${pkmE}.png" height="auto"; width="${size}"; id="pok";>`
   } else if (pkm[5].innerHTML.indexOf("img") == -1) {
-    return pkm[5].innerHTML = `<img src="./images/pkm/${pkmE}.png" height="250"; width="auto"; id="pok";>`
+    return pkm[5].innerHTML = `<img class="${pkmSize}" src="./images/pkm/${pkmE}.png" height="auto"; width="${size}"; id="pok";>`
   } else {
     return alert("Equipe cheia")
   }
@@ -55,15 +59,60 @@ function pronto() {
   body.style.overflow = "hidden"
 }
 
-function filtro() {
-  let checkbox = document.getElementById("filtroPkm")
-  let cbID = checkbox.value
+function filtro(filtro, gen1, gen2, gen3, gen4, gen5, gen6, gen7) {
+  let cbID = filtro.value
 
-  if (checkbox.checked) {
-    // return document.getElementById(`${cbID}`).style.display = 'none';
-    return document.getElementById(`row ${cbID}`).style.display = 'block';
-  } else {
-    // return document.getElementById(`${cbID}`).style.display = 'block';
-    return document.getElementById(`row ${cbID}`).style.display = 'none';
+  switch (cbID) {
+    case "gen1":
+      if (filtro.checked == false) {
+        document.querySelector(".gen1").innerHTML = "-"
+      } else {
+        document.querySelector(".gen1").innerHTML = gen1
+      }
+      break
+    case "gen2":
+      if (filtro.checked == false) {
+        document.querySelector(".gen2").innerHTML = "-"
+      } else {
+        document.querySelector(".gen2").innerHTML = gen2
+      }
+      break
+    case "gen3":
+      if (filtro.checked == false) {
+        document.querySelector(".gen3").innerHTML = "-"
+      } else {
+        document.querySelector(".gen3").innerHTML = gen3
+      }
+      break
+    case "gen4":
+      if (filtro.checked == false) {
+        document.querySelector(".gen4").innerHTML = "-"
+      } else {
+        document.querySelector(".gen4").innerHTML = gen4
+      }
+      break
+    case "gen5":
+      if (filtro.checked == false) {
+        document.querySelector(".gen5").innerHTML = "-"
+      } else {
+        document.querySelector(".gen5").innerHTML = gen5
+      }
+      break
+    case "gen6":
+      if (filtro.checked == false) {
+        document.querySelector(".gen6").innerHTML = "-"
+      } else {
+        document.querySelector(".gen6").innerHTML = gen6
+      }
+      break
+    case "gen7":
+      if (filtro.checked == false) {
+        document.querySelector(".gen7").innerHTML = "-"
+      } else {
+        document.querySelector(".gen7").innerHTML = gen7
+      }
+      break
   }
+
+  return false
 }
